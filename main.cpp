77643f3b4289641bfa2fb558cf398ea7b53f81cc
BenTheElder <bentheelder@gmail.com>
2018-02-12 01:33:35 -0800

    smaller protocol

--- main.cpp
+++ main.cpp
@@ -31,6 +31,8 @@ limitations under the License.
 // hc-sr04 ultrasonic ranger for water level
 #define WATER_HCSR04_TRIG_PIN p22
 #define WATER_HCSR04_ECHO_PIN p23
+// pin wired to nR reset pin on the NXP LPC1768
+#define RESET_PIN p6
 
 
 // Watchdog class based on
@@ -71,9 +73,14 @@ bool starts_with(const char *pre, const char *str) {
 Watchdog wdt;
 
 DigitalOut heater(HEATER_PIN);
+
 DS1820 temp_probe(TEMPERATURE_PROBE_PIN);
+double temperature;
+
 HCSR04 water_level_sensor(WATER_HCSR04_TRIG_PIN, WATER_HCSR04_ECHO_PIN);
 double water_distance_inches;
+
+DigitalInOut reset_pin(RESET_PIN);
 
 Serial pc(USBTX, USBRX);
 // serial receive buffer
@@ -83,17 +90,22 @@ char recv_buff[RECEIVE_BUFF_SIZE];
 // For debug only, this is an LED on the mbed device.
 DigitalOut led1(LED1);
 
+
+void update_temperature() {
+    temp_probe.convertTemperature(true, DS1820::this_device);
+    temperature = temp_probe.temperature();
+}
+
 void update_water_level() {
     water_distance_inches = water_level_sensor.read_inches();
 }
 
-
 bool probably_has_water() {
     double d = water_distance_inches;
     // 7.5 inches is roughly the real maximum
-    // after ~8.2 we can guess that the steam has skewed the measurement
+    // after ~9.4 we can guess that the steam has skewed the measurement
     // after 20 (most arbitrary) the steam is intense and we should back off
-    return (d < 7.5) || (d > 8.2 && d < 20);
+    return (d < 7.5) || (d > 9.4 && d < 20);
 }
 
 
@@ -101,40 +113,49 @@ bool probably_has_water() {
 Methods for replying with statuses for varioius sensors
 */
 void send_water() {
-    pc.printf("WATER+%.2f\n", water_distance_inches);
+    pc.printf("W+%.2f\n", water_distance_inches);
 }
 
 void send_temp() {
-    // read temp and reply with temperature
-    temp_probe.convertTemperature(true, DS1820::this_device);
-    double temperature = temp_probe.temperature();
-    pc.printf("TEMP+%.1f\n", temperature);
+    pc.printf("T+%.1f\n", temperature);
 }
 
 void send_brew() {
     if (heater) {
-        pc.printf("BREW+1\n");
+        pc.printf("B+1\n");
     } else {
-        pc.printf("BREW+0\n");
+        pc.printf("B+0\n");
     }
 }
+
+void reset() {
+    reset_pin.mode(OpenDrain);
+}
+
+#define COMMAND_STATUS       "S"
+#define COMMAND_WATER_LEVEL  "W"
+#define COMMAND_TEMP         "T"
+#define COMMAND_BREW         "B"
 
 // process_line handles one line of input and returns true if the line
 // was valid / handled and WDT should be reset
 bool process_line() {
-    if (starts_with("STATUS+?", recv_buff)) {
-        pc.printf("STATUS+1\n");
+    if (starts_with("RESET", recv_buff)) {
+        reset();
+
+    } else if (starts_with("S+?", recv_buff)) {
+        pc.printf("S+1\n");
         send_water();
         send_temp();
         send_brew();
 
-    } else if (starts_with("WATER+?", recv_buff)) {
+    } else if (starts_with("W+?", recv_buff)) {
         send_water();
 
-    } else if (starts_with("TEMP+?", recv_buff)) {
+    } else if (starts_with("T+?", recv_buff)) {
         send_temp();
 
-    } else if (starts_with("BREW+", recv_buff)) {
+    } else if (starts_with("B+", recv_buff)) {
         // update heater setting and reply with heater value
         // ignore input and don't leave the heater on if there is no water
         bool new_heater = (recv_buff[5] != '0') && probably_has_water();
@@ -155,6 +176,7 @@ int main() {
     // zero other vars
     memset(recv_buff, 0, RECEIVE_BUFF_SIZE);
     water_distance_inches = std::numeric_limits<double>::max();
+    temperature = std::numeric_limits<double>::max();
 
     // Initialization, set up watchdog, serial, etc.
     pc.baud(115200);
@@ -166,13 +188,15 @@ int main() {
     // loop vars
     // NOTE: if we poll the HCSR04 too fast the readings are useless
     update_water_level();
-    RateLimiter water_level_sensor_limiter(5000, update_water_level);
+    update_temperature();
+    RateLimiter water_level_sensor_rate_limiter(5000, update_water_level);
+    RateLimiter temperature_sensor_rate_limiter(100000, update_temperature);
     // current location in the receive buffer
     char *curr_buff = recv_buff;
     while (true) {
         // poll sensors
-        // NOTE: *not* the temperature, which is slow to read
-        water_level_sensor_limiter.call();
+        water_level_sensor_rate_limiter.call();
+        temperature_sensor_rate_limiter.call();
 
         // handle input
         bool received_newline = false;
